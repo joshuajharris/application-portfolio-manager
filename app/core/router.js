850798ec6cda4e218979b2403ac14454212d7658
Joshua Harris <joshua.jharris13@gmail.com>
2015-07-22 19:30:26 -0400

    initial commit

---
+++ app/core/router.js
@@ -0,0 +1,27 @@
+var restify = require('restify'),
+    fs = require('fs');
+
+var controllers = {},
+    controllers_path = process.cwd() + '/app/controllers';
+fs.readdirSync(controllers_path).forEach(function(file){
+  if(/.*(.js)$/.test(file)) {
+    controllers[file.split('.')[0]] = require(controllers_path + '/' + file);
+  }
+});
+
+var server = restify.createServer();
+
+server
+  .use(restify.fullResponse())
+  .use(restify.bodyParser());
+
+server.get('/', controllers.test.home);
+
+var port = process.env.PORT || 3000;
+server.listen(port, function(err) {
+  if(err) {
+    console.error(err);
+  } else {
+    console.log("Server listening on port 3000");
+  }
+});
