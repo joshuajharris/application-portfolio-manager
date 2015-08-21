7c2d7c18ef4ad7cb98125860aadb69b165b1586f
Joshua Harris <joshua.jharris13@gmail.com>
2015-08-20 23:39:14 -0400

    github projects are grabbed and stored in firebase and can be deleted on site

--- js/main.js
+++ js/main.js
@@ -1,33 +1,43 @@
-var apm = new Firebase('https://joshuajharris-apm.firebaseio.com/');
+var ghUserName = 'joshuajharris'; 
+var refRootUrl = 'https://joshuajharris-apm.firebaseio.com/';
+var ref = new Firebase(refRootUrl + 'projects');
 
-function getApps(db) {
-  db.on("value", function(snapshot){
-    var projects = snapshot.val();
-    $.each(projects, function(i, project) {
-      $('#projects').append(
-        '<div>' +
-          '<h3>' + project.name + '</h3>' +
-          'Clone Url: <code>' + project.git_url + '</code>' +
-          '<p>' + project.description + '</p>' +
-          '<button class="btn btn-danger">Remove Project</button>' +
-        '</div>'
-      );
-    });
-  }, function(errorObject){
-    console.log("The read failed: " + errorObject.code);
+ref.on("value", function(snapshot) {
+  var projects = snapshot.val();
+  
+  $.each(projects, function(i, project) {
+    $('#projects').append(
+      '<div id="' + project.id + '">' +
+        '<h3><i class="fa fa-github"></i> ' + project.name + '</h3>' +
+        'Clone Url: <code>' + project.git_url + '</code>' +
+        '<p>' + project.description + '</p>' +
+        '<button class="btn btn-danger" onclick="removeProject(\'' + project.id + '\')"><i class="fa fa-times"></i> Remove Project</button>' +
+      '</div>'
+    );
   });
+}, function(errorObject){
+  console.log("The read failed: " + errorObject.code);
+});
+
+ref.on("child_removed", function(snapshot){
+  $("#projects").empty();
+  console.log("child " + snapshot.key() + " removed");
+});
+
+function removeProject(projectId) {
+  ref.child(projectId).remove();
 }
 
-function pushGithubRepos(db, githubAPI) {
-  $.getJSON(githubAPI, function(json){
+function pushGithubRepos(userName) {
+  $.getJSON('https://api.github.com/users/' + ghUserName + '/repos', function(json) {
     $.each(json, function(i, repo) {
-      db.push(repo);
+      newProject = ref.push(repo);
+      newId = newProject.key();
+      ref.child(newId).update({id: newId});
     });
 
-    db.on("child_added", function(){
+    ref.on("child_added", function() {
       console.log("repo pushed");
     });
   });
 }
-
-getApps(apm);
